b061b72db8f23242098b509fa5b51a86c7da7ce0
Renzo Damian <renzo.damian.go@gmail.com>
2023-03-01 22:17:16 -0500

    E14: Extend interface

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -5,26 +5,26 @@ import { Equal, Expect } from "./helpers/type-utils";
  * interfaces. Can you find a way to refactor this to
  * make it more DRY?
  */
-
-interface User {
+interface Base {
   id: string;
+}
+
+interface User extends Base {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends Base {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends Base {
   comment: string;
 }
 
 type tests = [
   Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
   Expect<Equal<Post, { id: string; title: string; body: string }>>,
-  Expect<Equal<Comment, { id: string; comment: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>
 ];
